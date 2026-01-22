10b9fd81840648bfe23e3d3a61e7c68fc53cdba2
reeceappling <reeceappling@gmail.com>
2026-01-22 15:46:20 -0500

    cleanup some scripts

--- main_run.sh
+++ main_run.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+initialDir=$(pwd)
+workingDir=$(git rev-parse --show-toplevel)
+cd workingDir
+
 rm -rf bin/mushApi
-# -x on build?
-GOOS=linux GOARCH=arm64 go build -o bin/mushApi .
+GOOS=linux GOARCH=arm64 go build -o bin/mushApi rfid
 docker compose --env-file env/.env.devhttp up --build --force-recreate
+cd $initialDir
