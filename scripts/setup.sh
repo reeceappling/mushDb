78614b48edf63084d84b7ba526723cb3c0366497
reeceappling <reeceappling@gmail.com>
2026-05-28 14:29:16 -0400

    cleanup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,6 @@ initialDir=$(pwd)
 workingDir=$(git rev-parse --show-toplevel)
 cd $workingDir
 mkdir -p ~/mush/pictures
-cp testImages/test.jpg ~/mush/pictures/test.jpg
 chown -R $(id -u):$(id -g) ~/mush/pictures
 chmod -R 777 ~/mush/pictures
 cd $initialDir
