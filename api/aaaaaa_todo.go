d3fcbc71e4708f009304dc203a3ab4ba58825880
reeceappling <reeceappling@gmail.com>
2026-07-26 13:10:10 -0400

    add a silly go file that probably shouldnt be there but oh well

---
+++ api/aaaaaa_todo.go
@@ -0,0 +1,5 @@
+package api
+
+//go:generate ./goGenerator/mygenerator
+
+// TODO: add orgs? Owners of each item? Want it to be extensible to allow multiple users on the internet to utilize the system...
