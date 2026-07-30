f87286d3baf37d994021c9c394943eb062602690
reeceappling <reeceappling@gmail.com>
2026-07-30 18:55:54 -0400

    eod

--- api/env/env.go
+++ api/env/env.go
@@ -18,35 +18,35 @@ func GetEnv(ctx context.Context) string {
 func SetEnv(ctx context.Context, env string) context.Context {
 	return context.WithValue(ctx, envCtxKey, env)
 }
-func IfDev(ctx context.Context, doIfDev func() error) error { // TODO: use this everywhere needed
+func IfDev(ctx context.Context, doIfDev func() error) error {
 	if GetEnv(ctx) == Dev {
 		return doIfDev()
 	}
 	return nil
 }
-func IfCert(ctx context.Context, doIfProd func() error) error { // TODO: use this everywhere needed
+func IfCert(ctx context.Context, doIfProd func() error) error {
 	if GetEnv(ctx) == Cert {
 		return doIfProd()
 	}
 	return nil
 }
-func IfProd(ctx context.Context, doIfProd func() error) error { // TODO: use this everywhere needed
+func IfProd(ctx context.Context, doIfProd func() error) error {
 	if GetEnv(ctx) == Prod {
 		return doIfProd()
 	}
 	return nil
 }
-func IfNotProd(ctx context.Context, doIfProd func() error) error { // TODO: use this everywhere needed
+func IfNotProd(ctx context.Context, doIfProd func() error) error {
 	if GetEnv(ctx) != Prod {
 		return doIfProd()
 	}
 	return nil
 }
-func LogIfDev(ctx context.Context, toLog string) { // TODO: use this everywhere needed
+func LogIfDev(ctx context.Context, toLog string) {
 	if GetEnv(ctx) == Dev {
 		println(toLog)
 	}
 }
-func LogAlways(toLog string) { // TODO: use this everywhere needed
+func LogAlways(toLog string) {
 	println(toLog)
 }
