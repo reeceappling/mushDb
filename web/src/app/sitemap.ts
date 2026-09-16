edd8f5ad10987e916747088aae469c4bf93c273c
reeceappling <reeceappling@gmail.com>
2026-09-16 17:54:06 -0400

    sitemap stuff

--- web/src/app/sitemap.ts
+++ web/src/app/sitemap.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from 'next'
+import type {MetadataRoute} from 'next'
 import {BaseExternalUrl} from "@/app/components/Constants";
 
 // TODO: use these maybe once fixed!
 export async function generateSitemaps() {
     // Fetch the total number of products and calculate the number of sitemaps needed
     // TODO: FIX!
-    return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }]
+    return [{id: 0}, {id: 1}, {id: 2}, {id: 3}]
 }
 
 // TODO: use these maybe once fixed!
@@ -20,14 +20,18 @@ export default async function sitemap(props: {
     // const products = await getProducts(
     //     `SELECT id, date FROM products WHERE id BETWEEN ${start} AND ${end}`
     // )
-    const listTyps = ["agarBatches","agarRecipes","bags","fruits","fruitingChambers","grainBatches","jars","jarRecipes","lcs","lcRecipes","lcSyringes","mss","pcRuns","plates","plugs","projects","sales","slants","species","sporePrints","sporeSwabs","stasisTubes","subspecies","substrateBatches","substrateRecipes","transfers","users","waterJars"]
-    const importTyps = ["bag","fruit","fruitingChamber","jar","lc","lcSyringe","mss","plate","plugs","slant","sporePrint","sporeSwab","stasisTube","waterJar"]
-    const newTyps = ["agarRecipe","jarRecipe","lcRecipe","pcRun", "plugs", "project", "species", "subspecies", "substrateRecipe", "waterJar"]
+    const listTyps = ["agarBatches", "agarRecipes", "bags", "fruits", "fruitingChambers", "grainBatches", "jars", "jarRecipes", "lcs", "lcRecipes", "lcSyringes", "mss", "pcRuns", "plates", "plugs", "projects", "sales", "slants", "species", "sporePrints", "sporeSwabs", "stasisTubes", "subspecies", "substrateBatches", "substrateRecipes", "transfers", "users", "waterJars"]
+    const importTyps = ["bag", "fruit", "fruitingChamber", "jar", "lc", "lcSyringe", "mss", "plate", "plugs", "slant", "sporePrint", "sporeSwab", "stasisTube", "waterJar"]
+    const newTyps = ["agarRecipe", "jarRecipe", "lcRecipe", "pcRun", "plugs", "project", "species", "subspecies", "substrateRecipe", "waterJar"]
+    const policiesPages = ["cookies","privacy","security"] // TODO: ensure list is complete
+    const publicFiles = ["security.txt", "robots.txt", "staticContent/securityAcknowledgements.txt"] // TODO: ensure list is complete
+
     return [
         { // Main page
-            url: BaseExternalUrl||"mush.appli.ng",
+            url: BaseExternalUrl || "mush.appli.ng",
+            changeFrequency: "never",
             //lastModified: // TODO: ?????
-            //priority: // TODO:?????
+            priority: 1.0
             // alternates?: { // TODO: ???
             //     languages?: Languages<string> | undefined
             // } | undefined,
@@ -35,9 +39,10 @@ export default async function sitemap(props: {
             // videos?: Videos[] | undefined,// TODO: ???
         },
         { // Login page
-            url: BaseExternalUrl + "/login",
+            url:  `${BaseExternalUrl}/login`,
+            changeFrequency: "never",
             //lastModified: // TODO: ?????
-            //priority: // TODO:?????
+            priority: 0.5 // TODO: ?????
             // alternates?: { // TODO: ???
             //     languages?: Languages<string> | undefined
             // } | undefined,
@@ -45,11 +50,12 @@ export default async function sitemap(props: {
             // videos?: Videos[] | undefined,// TODO: ???
         },
         // List pages
-        ...listTyps.map(typ=>{
+        ...listTyps.map(typ => {
             return {
-            url: BaseExternalUrl + "/list/" + typ,
+                url: `${BaseExternalUrl}/list/${typ}`,
+                //changeFrequency: "always" // TODO: ???
                 //lastModified: // TODO: ?????
-                //priority: // TODO:?????
+                priority: 0.6 // TODO: ?????
                 // alternates?: { // TODO: ???
                 //     languages?: Languages<string> | undefined
                 // } | undefined,
@@ -58,28 +64,48 @@ export default async function sitemap(props: {
             }
         }),
         // Import pages
-        ...importTyps.map(typ=>{
+        ...importTyps.map(typ => {
             return {
-                url: BaseExternalUrl + "/import/" + typ,
+                url: `${BaseExternalUrl}/import/${typ}`,
+                changeFrequency: "never",
                 //lastModified: // TODO: ?????
-                //priority: // TODO:?????
+                priority: 0.6 // TODO: ?????
                 // alternates?: { // TODO: ???
                 //     languages?: Languages<string> | undefined
                 // } | undefined,
             }
         }),
         // Creation pages
-        ...newTyps.map(typ=>{
+        ...newTyps.map(typ => {
             return {
-                url: BaseExternalUrl + "/new/" + typ,
+                url: `${BaseExternalUrl}/new/${typ}`,
+                changeFrequency: "never",
                 //lastModified: // TODO: ?????
-                //priority: // TODO:?????
+                priority: 0.5 // TODO: ?????
                 // alternates?: { // TODO: ???
                 //     languages?: Languages<string> | undefined
                 // } | undefined,
             }
+        }),
+        // TODO: view pages? (must generate at the beginning, probably dont do this)
+        // policies pages (cookies, privacy)
+        ...policiesPages.map(policyName => {
+            return {
+                url: `${BaseExternalUrl}/policies/${policyName}`,
+                changeFrequency: "never",
+
+                //lastModified: // TODO: ?????
+                priority: 0.1
+            }
+        }),
+        // Public static pages and documents
+        ...publicFiles.map(path => {
+            return {
+                url: `${BaseExternalUrl}/${path}`,
+                changeFrequency: "never",
+                //lastModified: // TODO: ?????
+                priority: 0.1
+            }
         })
-        // TODO: view pages?
-        // TODO: policies pages (cookies, privacy)
     ]
 }
